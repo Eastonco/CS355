53a8aa6c214861379f0a06d8df3dca22bcc5dbf4
Eastonco <eastonco@icloud.com>
2020-03-30 14:03:20 -0700

    🪐Added base files for SPS part 2

---
+++ SPS/HW4_part2.py
@@ -0,0 +1,76 @@
+import re
+from HW4_part1 import *
+
+def tokenize(s):
+    return re.findall("/?[a-zA-Z][a-zA-Z0-9_]*|[\[][a-zA-Z-?0-9_\s!][a-zA-Z-?0-9_\s!]*[\]]|[-]?[0-9]+|[}{]+|%.*|[^ \t\n]", s)
+    
+
+
+# COMPLETE THIS FUNCTION
+# The it argument is an iterator.
+# The tokens between '{' and '}' is included as a sub code-array (dictionary). If the
+# parenteses in the input iterator is not properly nested, returns False.
+def groupMatch(it):
+    res = []
+    for c in it:
+        if c == '}':
+            return {'codearray':res}
+        elif c=='{':
+            # Note how we use a recursive call to group the tokens inside the
+            # inner matching parenthesis.
+            # Once the recursive call returns the code-array for the inner 
+            # parenthesis, it will be appended to the list we are constructing 
+            # as a whole.
+            res.append(groupMatch(it))
+        else:
+            res.append(c)
+    return False
+
+
+
+# COMPLETE THIS FUNCTION
+# Function to parse a list of tokens and arrange the tokens between { and } braces 
+# as code-arrays.
+# Properly nested parentheses are arranged into a list of properly nested dictionaries.
+def parse(L):
+    res = []
+    it = iter(L)
+    for c in it:
+        if c=='}':  #non matching closing parenthesis; return false since there is 
+                    # a syntax error in the Postscript code.
+            return False
+        elif c=='{':
+            res.append(groupMatch(it))
+        else:
+            res.append(c)
+    return {'codearray':res}
+
+# COMPLETE THIS FUNCTION 
+# This will probably be the largest function of the whole project, 
+# but it will have a very regular and obvious structure if you've followed the plan of the assignment.
+# Write additional auxiliary functions if you need them. 
+def interpretSPS(code): # code is a code array
+    pass
+
+
+def interpreter(s): # s is a string
+    interpretSPS(parse(tokenize(s)))
+
+
+#clear opstack and dictstack
+def clearStacks():
+    opstack[:] = []
+    dictstack[:] = []
+
+
+input1 = """
+            /square {dup mul} def
+            0 [-5 -4 3 -2 1]
+            {square add} forall
+            55 eq false and
+        """
+
+# print(tokenize(input1))
+# print(parse(tokenize(input1)))
+print(parse(['b', 'c', '{', 'a', '{', 'a', 'b', '}', '{', '{', 'e', '}', 'a', '}', '}']))
+
